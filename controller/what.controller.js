a4456426ce263854e45b80976281957a668e9fa0
AnmolV873 <vithalwalia01@gmail.com>
2025-06-06 11:52:50 +0530

    feat: Added two more api related to create session and create chat

--- controller/what.controller.js
+++ controller/what.controller.js
@@ -1,5 +1,5 @@
 import whatService from "../services/what.service.js";
-import  FormData from "form-data";
+import FormData from "form-data";
 
 const getWhat = async (req, res, next) => {
     try {
@@ -24,7 +24,7 @@ const createPersonality = async (req, res) => {
         console.log("formData", req.body);
         const formData = new FormData();
         formData.append("name", req.body.name);
-        
+
         if (req.file) {
             formData.append("avatar", req.file.buffer, {
                 filename: req.file.originalname,
@@ -47,4 +47,45 @@ const createPersonality = async (req, res) => {
     }
 };
 
-export { getWhat, createPersonality };
+const createChat = async (req, res, next) => {
+    try {
+        const { sessionId, message, userId, personalityId, wordLimit, modelName, language } = req.body;
+        // console.log("Request body in createChat controller :", req.body);
+        // console.log("Session ID in createChat controller:", sessionId);
+        if (!sessionId || typeof sessionId !== 'string') {
+            return res.status(400).json({ message: "Invalid sessionId" });
+        }
+        const response = await whatService.createChat(sessionId, message, userId, personalityId, wordLimit, modelName, language);
+        // console.log("Response from createChat---------------------> :", response);
+        res.status(200).json({
+            data: response,
+            message: "Chat created successfully"
+        });
+    } catch (error) {
+        console.error("Error in createChat controller:", error);
+        res.status(500).json({
+            data: null,
+            message: "Internal Server Error"
+        });
+    }
+}
+
+const createSession = async (req, res, next) => {
+    try {
+        const { userId, personalityId } = req.body;
+        const response = await whatService.createSession(userId, personalityId);
+        console.log("Response from createSession:", response);
+        res.status(200).json({
+            data: response,
+            message: "Session created successfully"
+        });
+    } catch (error) {
+        console.error("Error in createSession controller:", error);
+        res.status(500).json({
+            data: null,
+            message: "Internal Server Error"
+        });
+    }
+}
+
+export { getWhat, createPersonality, createChat, createSession };
